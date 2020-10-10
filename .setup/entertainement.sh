f1af53e1d2e5cd54cc209635e7b8dec21ddf1c73
Hassen Taidirt <htaidirt@gmail.com>
2020-10-10 20:20:33 +0200

    Correctly config Spotifyd

--- .setup/entertainement.sh
+++ .setup/entertainement.sh
@@ -5,3 +5,7 @@ source ./helpers.sh
 yinstall spotifyd
 yinstall spotify-tui
 
+systemctl --user enable spotifyd --now
+# For a better understanding on configuring spotifyd and spotify-tui
+# visit https://www.rockyourcode.com/spotify-in-the-terminal-with-spotify-tui-and-spotifyd/
+
